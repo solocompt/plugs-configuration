fa2537e639e019c94b1318dfd4a72fe401c08d6f
Ricardo Lobo <ricardolobo@audienciazero.org>
2019-04-08 13:24:17 +0100

    adds on_delete

--- plugs_configuration/models.py
+++ plugs_configuration/models.py
@@ -22,7 +22,7 @@ class Configuration(mixins.Timestampable, models.Model):
     key = models.CharField(max_length=20)
     value = models.TextField()
     type = models.CharField(max_length=10, choices=TYPES, default=EXTERNAL)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.PROTECT)
 
     def validate_unique(self, exclude=None):
         if self.type != Configuration.USER and (
